2a558045054e558a9849539e4e49a11785cab838
Ashish Mahour <amashishmahour@gmail.com>
2018-11-07 16:01:40 +0530

    first git!!

---
+++ mvn-dynamic-web-boot-project/src/main/java/com/mdwbp/main/SpringMainClass.java
@@ -0,0 +1,13 @@
+package com.mdwbp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMainClass {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMainClass.class, args);
+	}
+
+}
